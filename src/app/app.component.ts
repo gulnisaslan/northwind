0c9457fe02b22668f4bbcda31daa10705f8b9e78
Gülnisa Aslan <gulnisaslan@gmail.com>
2021-03-13 14:44:33 +0300

    Update Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'northwind';
-  user: string = "Engin demiroğ";
+  //user: string = "Engin demiroğ";
 
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -17,20 +17,20 @@ export class ProductComponent implements OnInit {
  
     //ProductResponseModel:ProductResponseModule=[];
   
-  constructor(private productService:ProductService) { }
+   constructor(private productService:ProductService) { }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
     this.getProducts();
-  }
+    }
 
-  getProducts(){
-  this.productService.getProducts().subscribe(response=>{
+    getProducts(){
+    this.productService.getProducts().subscribe(response=>{
     this.products=response.data
     this.dataLoaded=true;
   
   })
    
-    
+  
     
 } 
 
